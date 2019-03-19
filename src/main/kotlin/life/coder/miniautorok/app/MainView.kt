812a56318b864692d4333440f677ca6f68d5cac5
Ngoc LE <ngoc.le@smarp.com>
2019-03-19 11:14:37 +0200

    Restart to work with Kotlin

---
+++ src/main/kotlin/life/coder/miniautorok/app/MainView.kt
@@ -0,0 +1,11 @@
+package life.coder.miniautorok.app
+
+import tornadofx.*
+
+class MainView : View("Hello TornadoFX") {
+    override val root = hbox {
+        label(title) {
+            addClass(Styles.heading)
+        }
+    }
+}
---
+++ src/main/kotlin/life/coder/miniautorok/app/MyApp.kt
@@ -0,0 +1,5 @@
+package life.coder.miniautorok.app
+
+import tornadofx.App
+
+class MyApp: App(MainView::class, Styles::class)
